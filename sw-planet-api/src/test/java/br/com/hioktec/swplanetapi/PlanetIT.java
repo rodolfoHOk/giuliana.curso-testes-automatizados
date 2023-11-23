041f93d8b3dfd2f58e08c6f14c59b9dac77965dd
Rodolfo HiOk <rodolfo.okino@gmail.com>
2023-11-23 20:50:20 -0300

    test: e2e config my sql database

--- sw-planet-api/src/test/java/br/com/hioktec/swplanetapi/PlanetIT.java
+++ sw-planet-api/src/test/java/br/com/hioktec/swplanetapi/PlanetIT.java
@@ -3,7 +3,9 @@ package br.com.hioktec.swplanetapi;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("it")
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class PlanetIT {
   
